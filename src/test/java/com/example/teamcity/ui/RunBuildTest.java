a235d643c5f240e7d706cf1620a316f4ebfea405
annabodrova <abodrova@hapicloud.io>
2024-02-21 12:23:22 +0300

    try catch $$

--- src/test/java/com/example/teamcity/ui/RunBuildTest.java
+++ src/test/java/com/example/teamcity/ui/RunBuildTest.java
@@ -1,10 +1,14 @@
 package com.example.teamcity.ui;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
 import com.example.teamcity.ui.pages.ProjectPage;
 import com.example.teamcity.ui.pages.ProjectsPage;
 import com.example.teamcity.ui.pages.admin.CreateNewProject;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selenide.$$;
 
 public class RunBuildTest extends BaseUITest {
     @Test
@@ -17,10 +21,20 @@ public class RunBuildTest extends BaseUITest {
                 .createProjectByUrl(url)
                 .setupProject(testData.getNewProjectDescription().getName(), testData.getBuildType().getName());
 
-        new ProjectsPage().open()
-                .getSubprojects()
-                .stream().reduce((first, second) -> second).get()
-                .openProject();
+//        new ProjectsPage().open()
+//                .getSubprojects()
+//                .stream().reduce((first, second) -> second).get()
+//                .openProject();
+
+        new ProjectsPage().open();
+        for (int i = 0; i< 3; i++ ) {
+            try {
+                $$(".Subproject__container--Px").get(0).click();
+                break;
+            } catch (StaleElementReferenceException e) {
+                Selenide.refresh();
+            }
+        }
 
         new ProjectPage()
                 .runBuild()
